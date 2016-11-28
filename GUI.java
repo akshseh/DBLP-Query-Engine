4c9842211c6415e773c2e47640226472a1bcb9fe
aksh98 <akarsha15010@iiitd.ac.in>
2016-11-28 23:53:44 +0530

    GUI mainPage

--- GUI.java
+++ GUI.java
@@ -5,6 +5,8 @@ import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -22,7 +24,7 @@ public class GUI
          {
             public void run()
             {
-               FontFrame frame = new FontFrame();
+               DBLP_GUI frame = new DBLP_GUI();
                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                frame.setVisible(true);
             }
@@ -31,43 +33,68 @@ public class GUI
 }
 
 //group layout 
-class FontFrame extends JFrame
+class DBLP_GUI extends JFrame
 {
-   public FontFrame()
+	private JComboBox queryChoice;
+	private JComboBox searchBy;
+	private JCheckBox sortByRelevance;
+	private JCheckBox sortByYear;
+	private JScrollPane pane;
+	private JTextArea queryResults;
+	private JTextArea nameTitleTags;
+   	private JTextArea sinceYear;
+   	private JTextArea customRangeTo;
+   	private JTextArea customRangeFrom;
+   	private JButton search;
+   	private JButton reset;
+   	
+   public DBLP_GUI()
    {
       setTitle("GUI");
-      setSize(600, 600);
+      setSize(700, 500);
 
-      ActionListener listener = new FontAction();
-
+      ActionListener listener = new DblpAction();
       // components
-
       JLabel queryLabel = new JLabel("Query: ");
-
-      queryChoice = new JComboBox(new String[] {"" ,"Query 1", "Query 2" });
-
+      queryChoice = new JComboBox(new String[] {"query" ,"Query 1", "Query 2" });
       queryChoice.addActionListener(listener);
 
       JLabel searchLabel = new JLabel("Search By: ");
-
-      searchBy = new JComboBox(new String[] { "","By relevance", "By date"});
-
+      searchBy = new JComboBox(new String[] { "search by","By relevance","By date"});
       searchBy.addActionListener(listener);
-
+      JLabel nameTitleTagsLabel = new JLabel("Name/Title Tags: ");
+      JLabel sinceYearLabel = new JLabel("Since Year: ");
+      JLabel rangeLabel = new JLabel("Custom Range: ");
+      customRangeFrom = new JTextArea();
+      customRangeTo = new JTextArea();
+      
+      customRangeTo.setEditable(true);
+      customRangeTo.setText("YYYY");
+      customRangeFrom.setText("YYYY");
+      customRangeFrom.setEditable(true);
+      
+      //customRange.addActionListener();
+      sinceYear = new JTextArea();
+      sinceYear.setEditable(true);
+      sinceYear.setText("YYYY");
+     // sinceYear.addActionListener();
       sortByRelevance = new JCheckBox("Sort By Relevance");
-      sortByRelevance.addActionListener(listener);
+    //  sortByRelevance.addActionListener(listener);
 
       sortByYear = new JCheckBox("Sort By Year");
-      sortByYear.addActionListener(listener);
+     // sortByYear.addActionListener(listener);
       nameTitleTags = new JTextArea();
-      sample = new JTextArea();
-      sample.setText("No queries to be displayed ! ");
-      sample.setEditable(false);
-      sample.setLineWrap(true);
-      sample.setBorder(BorderFactory.createEtchedBorder());
-
-      pane = new JScrollPane(sample);
-
+      queryResults = new JTextArea();
+      queryResults.setText("No queries to be displayed ! ");
+      queryResults.setEditable(false);
+      queryResults.setLineWrap(true);
+      queryResults.setBorder(BorderFactory.createEtchedBorder());
+      
+      pane = new JScrollPane(queryResults);
+      search = new JButton("Search");
+      //search.addActionListener();
+      reset = new JButton("reset");
+      //reset.addActionListener();
       GroupLayout layout = new GroupLayout(getContentPane());
       setLayout(layout);
       
@@ -77,13 +104,29 @@ class FontFrame extends JFrame
             				.addGroup(GroupLayout.Alignment.TRAILING,layout.createSequentialGroup()
             						.addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
             								.addComponent(queryLabel)
-            								.addComponent(searchLabel)).addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                                    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
+            								.addComponent(searchLabel)
+            								.addComponent(nameTitleTagsLabel)
+            								.addComponent(sinceYearLabel)
+            								.addComponent(rangeLabel)
+            								.addComponent(search)).addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                    .addGroup((layout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
                                           	.addComponent(searchBy)
-                                            .addComponent(queryChoice))
-                                    )
-            				.addComponent(sortByYear).
-            				addComponent(sortByRelevance)).addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                            .addComponent(queryChoice)
+                                            .addComponent(nameTitleTags)
+                                            .addComponent(sinceYear)
+                                            .addComponent(customRangeTo)
+                                            .addComponent(customRangeFrom))
+                                            //.addComponent(customRangeTo)
+                                            .addComponent(reset)
+                                            ).addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                    //.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
+                                      //    	.addComponent(customRangeFrom)
+                                        //  	)
+                                     )
+            				.addComponent(sortByYear)
+            				.addComponent(sortByRelevance)
+            				//.addGap(200)
+            				).addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
             		.addComponent(pane).addContainerGap()
             		)
             );
@@ -101,24 +144,30 @@ class FontFrame extends JFrame
                                     .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
                                           .addComponent(searchBy)
                                           .addComponent(searchLabel)).addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                            .addComponent(nameTitleTagsLabel)
+                                            .addComponent(nameTitleTags)).addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                            .addComponent(sinceYearLabel)
+                                            .addComponent(sinceYear)).addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                            .addComponent(rangeLabel)
+                                            .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                            		.addComponent(customRangeFrom)
+                                            		.addComponent(customRangeTo))).addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
                                     .addComponent(sortByYear, GroupLayout.DEFAULT_SIZE,GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE).addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                                    .addComponent(sortByRelevance, GroupLayout.DEFAULT_SIZE,GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                                    .addComponent(sortByRelevance, GroupLayout.DEFAULT_SIZE,GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addGroup(layout.createParallelGroup(Alignment.BASELINE)
+                                    		.addComponent(search)
+                                    		.addComponent(reset)).addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                   .addGap(200)
+                                    ))
                         .addContainerGap()
                         )
             );
    }
-
-   private JComboBox queryChoice;
-   private JComboBox searchBy;
-   private JCheckBox sortByRelevance;
-   private JCheckBox sortByYear;
-   private JScrollPane pane;
-   private JTextArea sample;
-   private JTextArea nameTitleTags;
-   
-
  
-  private class FontAction implements ActionListener
+  private class DblpAction implements ActionListener
    {
       public void actionPerformed(ActionEvent event)
       {
